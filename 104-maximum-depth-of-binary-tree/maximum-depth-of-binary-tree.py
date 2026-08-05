69d7d78235c340ea3713982ab2e9977cec2bcf2d
Jaideep Reddy <veeramjaideepreddy@gmail.com>
2026-08-06 00:51:59 +0530

    Time: 9 ms (24.29%) | Memory: 24.7 MB (43.49%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxDepth(self, root):
+        if root is None:
+            return 0
+
+        left = self.maxDepth(root.left)
+
+        right = self.maxDepth(root.right)
+
+        return 1 + max(left, right)
