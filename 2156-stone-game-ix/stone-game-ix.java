0c784dd0173009823911b04e939af64bfdd8e28c
Jaideep Reddy <veeramjaideepreddy@gmail.com>
2026-08-16 21:51:31 +0530

    Time: 3 ms (100.00%) | Memory: 114.7 MB (82.43%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int[] count = new int[3];
+        for (int v : stones) count[v % 3]++;
+
+        if (count[0] % 2 == 0) {
+            return count[1] > 0 && count[2] > 0;
+        }
+        return Math.abs(count[1] - count[2]) > 2;
+    }
+}
